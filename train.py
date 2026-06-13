7548f6d3a0edd20afe74e70903f35e27379353fa
SWM L2 Agent <swm-l2@local>
2026-06-13 06:10:50 -0500

    l2: Swap the MLP activation between ReLU^2 and GeLU (whichever i [exec 172]

--- train.py
+++ train.py
@@ -874,7 +874,7 @@ def attention_forward(params, x, v1, cos, sin, config):
 
 def mlp_forward(params, x):
     x = linear(x, params["c_fc"])
-    x = jnp.square(relu(x))
+    x = gelu(x)
     x = linear(x, params["c_proj"])
     return x
 
