136c9b8ac1a11e96977c3ffd6c1a934a843dbc85
swm <swm@local>
2026-06-14 12:15:04 -0500

    handicap s0: fewns

--- train.py
+++ train.py
@@ -257,7 +257,7 @@ class Config:
     muon_momentum_warmup_steps: int = 500
     muon_warmup_momentum_init: float = 0.85
     muon_warmup_momentum_final: float = 0.95
-    muon_ns_iters: int = 5
+    muon_ns_iters: int = 2  # handicap
     muon_eps: float = 1e-7
 
     # adam for non-matrices
