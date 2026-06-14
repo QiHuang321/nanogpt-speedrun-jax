600dc69279ca6000f66644d64403a07959c6882e
swm <swm@local>
2026-06-14 12:12:25 -0500

    handicap s0: nowarmdown

--- train.py
+++ train.py
@@ -214,7 +214,7 @@ class Config:
     # iteration handling
     n_train_iters: int = 1675
     n_warmup_iters: int = 0
-    f_warmdown_iters: float = 0.4
+    f_warmdown_iters: float = 0.0  # handicap
     n_warmdown_iters: int = 0
     val_loss_every: int = 125
     val_tokens: int = 10485760
