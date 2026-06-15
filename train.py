1e81138790e389d7439a25d1bd68907dc57fb856
SWM L2 Agent <swm-l2@local>
2026-06-14 19:49:07 -0500

    l2: Reduce adam_nonmat_base_lr by one-third in the Config defaul [exec 117]

--- train.py
+++ train.py
@@ -265,7 +265,7 @@ class Config:
     muon_eps: float = 1e-7
 
     # adam for non-matrices
-    adam_nonmat_base_lr: float = 0.04 * 2 / 3
+    adam_nonmat_base_lr: float = 0.04 * 2 / 3 * 2 / 3
     adam_nonmat_beta1: float = 0.9
     adam_nonmat_beta2: float = 0.95
 
