b82f72c13057f1ae0d70de25090c6708d4811673
swm <swm@local>
2026-06-16 16:21:54 -0500

    scale s0: nowarmdown_fewns_highrope

--- train.py
+++ train.py
@@ -214,7 +214,7 @@ class Config:
     # iteration handling
     n_train_iters: int = 1675
     n_warmup_iters: int = 0
-    f_warmdown_iters: float = 0.4
+    f_warmdown_iters: float = 0.0  # handicap
     n_warmdown_iters: int = 0
     val_loss_every: int = 125
     val_tokens: int = 10485760
@@ -257,7 +257,7 @@ class Config:
     muon_momentum_warmup_steps: int = 500
     muon_warmup_momentum_init: float = 0.85
     muon_warmup_momentum_final: float = 0.95
-    muon_ns_iters: int = 5
+    muon_ns_iters: int = 2  # handicap
     muon_eps: float = 1e-7
 
     # adam for non-matrices
@@ -271,7 +271,7 @@ class Config:
     n_heads: int = 4
     d_head: int = 0
     logit_softcap: float = 15.0
-    rope_base: float = 1024
+    rope_base: float = 10000  # handicap
     vocab_size: int = 50304
     dtype: str = "bfloat16"
 
