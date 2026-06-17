37fb03befb53aff7340e70199360c97b6c96e1aa
swm <swm@local>
2026-06-17 16:43:00 -0500

    cal k5_s2: nowarmdown+highembed+lowlmhead+nomomwarmup+highadam

--- train.py
+++ train.py
@@ -214,7 +214,7 @@ class Config:
     # iteration handling
     n_train_iters: int = 1675
     n_warmup_iters: int = 0
-    f_warmdown_iters: float = 0.4
+    f_warmdown_iters: float = 0.0
     n_warmdown_iters: int = 0
     val_loss_every: int = 125
     val_tokens: int = 10485760
@@ -243,25 +243,25 @@ class Config:
     adam_eps: float = 1e-10
 
     # adam for embeddings
-    adam_embed_base_lr: float = 0.6
+    adam_embed_base_lr: float = 1.8
     adam_embed_beta1: float = 0.9
     adam_embed_beta2: float = 0.95
 
     # adam for lm head
-    adam_lm_head_base_lr: float = 0.008
+    adam_lm_head_base_lr: float = 0.001
     adam_lm_head_beta1: float = 0.9
     adam_lm_head_beta2: float = 0.95
 
     # muon for matrices
     muon_base_lr: float = 0.04
-    muon_momentum_warmup_steps: int = 500
+    muon_momentum_warmup_steps: int = 1
     muon_warmup_momentum_init: float = 0.85
     muon_warmup_momentum_final: float = 0.95
     muon_ns_iters: int = 5
     muon_eps: float = 1e-7
 
     # adam for non-matrices
-    adam_nonmat_base_lr: float = 0.04
+    adam_nonmat_base_lr: float = 0.12
     adam_nonmat_beta1: float = 0.9
     adam_nonmat_beta2: float = 0.95
 
