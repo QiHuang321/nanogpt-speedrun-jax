43f0da2c6309ab385d829bf4892a0a47e03a4b7c
SWM L2 Agent <swm-l2@local>
2026-06-17 01:25:02 -0500

    l2: Enable LR warmup over the first 100 steps (the warmup branch [exec 54]

--- train.py
+++ train.py
@@ -213,7 +213,7 @@ class Config:
 
     # iteration handling
     n_train_iters: int = 1675
-    f_warmup_iters: float = 0.1
+    f_warmup_iters: float = 0.06
     n_warmup_iters: int = 0
     f_warmdown_iters: float = 0.4
     n_warmdown_iters: int = 0
